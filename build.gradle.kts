91463d5faeeeb9e61357cc315656bcc39f88f50b
Jordon de Hoog <jordon.dehoog@gmail.com>
2023-05-08 12:05:32 -0400

    update kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
         classpath("com.android.tools.build:gradle:8.0.1")
         classpath("io.deepmedia.tools:publisher:0.6.0")
     }
